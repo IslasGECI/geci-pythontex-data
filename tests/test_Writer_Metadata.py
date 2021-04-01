cbee68996ef574b3ed5e9dd396280b44bf58cec6
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-31 22:11:11 -0700

    ⚡️ Change to pytest-mock

--- tests/test_Writer_Metadata.py
+++ tests/test_Writer_Metadata.py
@@ -1,11 +1,10 @@
 import geci_pythontex_data as gpd
-from unittest import mock
 
 dictionary = {"resources": [{"description": "hola", "titulo": "Titulo", "drive": "Drive"}]}
 
 
-def test_buildes():
-    with mock.patch.object(gpd.Writer_Metadata, "_load_datapackage", return_value=dictionary):
+def test_buildes(mocker):
+    with mocker.patch.object(gpd.Writer_Metadata, "_load_datapackage", return_value=dictionary):
         writer = gpd.Writer_Metadata()
         writer.load_metadata("diccionario.json")
         description = writer.description()
