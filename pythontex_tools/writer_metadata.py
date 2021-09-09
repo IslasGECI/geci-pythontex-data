1ded04da4cdbc71e56281e0a669d789714e2f804
maritza <maritza.bello@islas.org.mx>
2021-09-09 13:10:06 -0700

    ✅ Pass tests

--- pythontex_tools/writer_metadata.py
+++ pythontex_tools/writer_metadata.py
@@ -1,27 +1,27 @@
 import json
 
-
 class Writer_Metadata:
-    def __init__(self):
+    def __init__(self,level=0):
         self._information = None
+        self.level = level
 
     def load_metadata(self, path):
         self._information = load_datapackage(path)
 
     @property
     def titulo(self):
-        return self._information["resources"][0]["titulo"]
+        return self._information["resources"][self.level]["titulo"]
 
     @property
     def link_to_drive(self):
-        return self._information["resources"][0]["drive"]
+        return self._information["resources"][self.level]["drive"]
 
     def write_title_with_link(self):
         title_with_link = f"\href{ {self.link_to_drive} }{ {self.titulo} }".replace("'", "")
         return title_with_link
 
     def description(self):
-        return self._information["resources"][0]["description"]
+        return self._information["resources"][self.level]["description"]
 
 
 def load_datapackage(path):
